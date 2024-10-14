3e9b61b6901cbd372d7b15aa87cb00f8ba77a3e3
Volodymyr <volodymyr2126@gmail.com>
2024-10-14 19:55:42 +0300

    Generics and option done!

--- rustlings/exercises/option/option2.rs
+++ rustlings/exercises/option/option2.rs
@@ -1,16 +1,18 @@
 // option2.rs
 // Make me compile! Execute `rustlings hint option2` for hints
 
-// I AM NOT DONE
 
 fn main() {
     let optional_word = Some(String::from("rustlings"));
     // TODO: Make this an if let statement whose value is "Some" type
-    word = optional_word {
-        println!("The word is: {}", word);
-    } else {
+    if optional_word.is_some(){ 
+        let word = optional_word;
+        println!("The word is: {}", word.unwrap());
+    }
+    else {
         println!("The optional word doesn't contain anything");
     }
+
 
     let mut optional_integers_vec: Vec<Option<i8>> = Vec::new();
     for x in 1..10 {
@@ -19,7 +21,8 @@ fn main() {
 
     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
     // You can stack `Option<T>`'s into while let and if let
-    integer = optional_integers_vec.pop() {
-        println!("current value: {}", integer);
-    }
+    while optional_integers_vec.get(0).is_some(){
+    let integer = optional_integers_vec.pop();
+    println!("current value: {}", integer.unwrap().unwrap());
+}
 }
